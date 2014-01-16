3d70aa52d42bf76ad712934bfb2330fb2a57bb5a
Samuel Szoniecky <samszon@gmail.com>
2014-01-16 12:04:58 +0100

    rotten tomato

---
+++ rt.php
@@ -0,0 +1,44 @@
+<?php
+	if (!function_exists('curl_init')){
+		die('CURL is not installed!');
+	}
+
+	if(isset($_GET["q"])){
+		$query=$_GET["q"];
+		$apikey = '';
+		$q = urlencode($query); // make sure to url encode an query parameters
+		
+		// construct the query with our apikey and the query we want to make
+		$endpoint = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' . $apikey . '&q=' . $q;
+		
+		// setup curl to make a call to the endpoint
+		$session = curl_init($endpoint);
+		
+		// indicates that we want the response back
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+		
+		// exec curl and get the data back
+		$data = curl_exec($session);
+		
+		// remember to close the curl session once we are finished retrieveing the data
+		curl_close($session);
+		
+		// decode the json data to make it easier to parse the php
+		echo $data;
+		/*
+		$search_results = json_decode($data);
+		if ($search_results === NULL) die('Error parsing json');
+		
+		// play with the data!
+		$movies = $search_results->movies;
+		echo '<ul>';
+		foreach ($movies as $movie) {
+		  echo '<li><a href="' . $movie->links->alternate . '">' . $movie->title . " (" . $movie->year . ")</a></li>";
+		}
+		echo '</ul>';
+		*/		
+	}
+	
+
+?>
+	
